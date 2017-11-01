fc077cb446d767f407106e0cb9bcaba12e51b9fe
Pedro Guedes <pedro.braga@ccc.ufcg.edu.br>
2017-11-01 21:18:18 -0200

    US01 finished

--- js/controllers.js
+++ js/controllers.js
@@ -4,11 +4,27 @@ angular.module("sistemaDeMusica", []);
 angular.module("sistemaDeMusica").controller("sistemaController", function($scope){
 	$scope.artistas = [];
 	$scope.adicionarArtista = function(artista){
-		$scope.artistas.push(angular.copy(artista)) //Cria uma copia do objeto artista e ela que sera adicionada ao array
+		if(existeArtista(artista)){
+			//Se ja existe, nao faz nada, nao adiciona
+		}else{
+			$scope.artistas.push(angular.copy(artista)) //Cria uma copia do objeto artista e ela que sera adicionada ao array
+			delete $scope.artista; //Deleta o artista que esta no $scope, a copia ja foi add no array
+		}
 	};
-	delete $scope.artista; //Deleta o artista que esta no $scope, a copia ja foi add no array
+	//funcao que retorna se o array esta populado ou nao
+	$scope.isPopulated = function(array){
+		return array.length > 0;
+	}
+
+	//Verifica se existe artista no array de artistas
+	function existeArtista(artista){
+		for(i = 0; i < $scope.artistas.length; i++){
+			if($scope.artistas[i].nome === artista.nome){
+				return true;
+			}
+		}
+		return false;
+	}
+
+
 });
-
-
-
-
