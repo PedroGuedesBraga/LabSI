ce2b5a461ce8c5ce1130d4e8cdda84d0f815e8e2
Pedro Guedes <pedro.braga@ccc.ufcg.edu.br>
2017-11-02 22:32:36 -0200

    US02 concluded

--- js/controllers.js
+++ js/controllers.js
@@ -3,11 +3,15 @@ angular.module("sistemaDeMusica", []);
 //$scope é um objeto que vai pertencer ao view e ao controller ao msm tempo
 angular.module("sistemaDeMusica").controller("sistemaController", function($scope){
 	$scope.artistas = [];
+	$scope.albuns = [];
+	$scope.musicas = []; //Array com todas as musicas do sistema
+
+
 	$scope.adicionarArtista = function(artista){
 		if(existeArtista(artista)){
-			//Se ja existe, nao faz nada, nao adiciona
+			alert("Artista já existente no sistema");
 		}else{
-			$scope.artistas.push(angular.copy(artista)) //Cria uma copia do objeto artista e ela que sera adicionada ao array
+			$scope.artistas.push(angular.copy(artista)); //Cria uma copia do objeto artista e ela que sera adicionada ao array
 			delete $scope.artista; //Deleta o artista que esta no $scope, a copia ja foi add no array
 		}
 	};
@@ -17,7 +21,7 @@ angular.module("sistemaDeMusica").controller("sistemaController", function($scop
 	}
 
 	//Verifica se existe artista no array de artistas
-	function existeArtista(artista){
+	existeArtista = function(artista){
 		for(i = 0; i < $scope.artistas.length; i++){
 			if($scope.artistas[i].nome === artista.nome){
 				return true;
@@ -25,6 +29,55 @@ angular.module("sistemaDeMusica").controller("sistemaController", function($scop
 		}
 		return false;
 	}
+
+	//Criacao do tipo album
+	function album(nomeDoAlbum){
+		this.musicas = []; //Array de musicas
+		this.nomeDoAlbum = nomeDoAlbum;
+
+		this.pushMusic = function(musica){//Add musica ao album, o album NAO pode ter 2 musicas com o mesmo nome
+			if(!containsMusica(this.musicas, musica.nome)){
+				this.musicas.push(musica);
+				$scope.musicas.push(musica);
+			}else{
+				alert("Musica ja existente no álbum")
+			}
+		}
+	}
+
+
+	//Metodo que verifica se existe uma musica em um dado array de musicas
+	containsMusica = function(arrayMusica, nomeDaMusica){
+		for(i = 0; i < arrayMusica.length; i++){
+			if(arrayMusica[i].nome == nomeDaMusica){
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+
+	//Verifica se existe um determinado album com determinado nome, se existir, retorna o album, se nao, retorna um novo album vazio
+	retornaAlbum = function(nomeDoAlbum){
+		for(i = 0; i < $scope.albuns.length; i++){
+			if($scope.albuns[i].nomeDoAlbum == nomeDoAlbum){
+				return $scope.albuns[i];
+			}
+		}
+		var novoAlbum = new album(nomeDoAlbum);
+		$scope.albuns.push(novoAlbum);
+		return novoAlbum;
+	}
+
+
+	//Adiciona uma musica a um album que está no array de albuns.
+	$scope.adicionaMusica = function(musica){
+		retornaAlbum(musica.album).pushMusic(angular.copy(musica));
+		delete musica;
+	}
+
+
 
 
 });
