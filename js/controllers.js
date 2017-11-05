dd2d790f59ae289d450973317b0d0fecc2b3c097
Pedro Guedes <pedro.braga@ccc.ufcg.edu.br>
2017-11-05 00:30:26 -0200

    US3 falta besteiras acho

--- js/controllers.js
+++ js/controllers.js
@@ -5,6 +5,18 @@ angular.module("sistemaDeMusica").controller("sistemaController", function($scop
 	$scope.artistas = [];
 	$scope.albuns = [];
 	$scope.musicas = []; //Array com todas as musicas do sistema
+	$scope.statusCaixaDePesquisaDeArtistas = true;
+	$scope.selected; //Variavel usada para se referir ao artista que se está tratando se quiser exibir mais informacoes (US03) - Ver: https://stackoverflow.com/questions/33904251/ng-repeat-does-not-work-in-bootstrap-modal
+
+	//Metodo usado para atualizar o select
+	$scope.select = function(artista){
+		$scope.selected = artista;
+	}
+
+
+	$scope.changeStatusCaixaPesquisa = function(){ //Usado no botão de pesquisar artistas, deixar visivel caixa de pesquisa
+		$scope.statusCaixaDePesquisaDeArtistas = !$scope.statusCaixaDePesquisaDeArtistas;
+	}
 
 
 	$scope.adicionarArtista = function(artista){
@@ -30,17 +42,31 @@ angular.module("sistemaDeMusica").controller("sistemaController", function($scop
 		return false;
 	}
 
+
+
+	//Atualizar artista - Ultima musica que o usuario ouviu e nota
+	$scope.atualizaArtista = function(selectedItem){
+		for(i = 0; i < $scope.artistas.length; i++){
+			if($scope.artistas[i].nome == selectedItem.nome){
+				$scope.artistas[i].nota = selectedItem.nota;
+				$scope.artistas[i].ultimaMusica = selectedItem.ultimaMusica;
+			}
+		}
+	}
+
+
 	//Criacao do tipo album
-	function album(nomeDoAlbum){
+	function Album(nomeDoAlbum, autorDoAlbum){
 		this.musicas = []; //Array de musicas
 		this.nomeDoAlbum = nomeDoAlbum;
+		this.autorDoAlbum = autorDoAlbum;
 
 		this.pushMusic = function(musica){//Add musica ao album, o album NAO pode ter 2 musicas com o mesmo nome
 			if(!containsMusica(this.musicas, musica.nome)){
 				this.musicas.push(musica);
 				$scope.musicas.push(musica);
 			}else{
-				alert("Musica ja existente no álbum")
+				alert("Musica ja existente no álbum");
 			}
 		}
 	}
@@ -59,23 +85,38 @@ angular.module("sistemaDeMusica").controller("sistemaController", function($scop
 
 
 	//Verifica se existe um determinado album com determinado nome, se existir, retorna o album, se nao, retorna um novo album vazio
-	retornaAlbum = function(nomeDoAlbum){
+	retornaAlbum = function(nomeDoAlbum, autor){ //Parametro autor (do album) usado somente se for necessario criar um novo album
 		for(i = 0; i < $scope.albuns.length; i++){
 			if($scope.albuns[i].nomeDoAlbum == nomeDoAlbum){
 				return $scope.albuns[i];
 			}
 		}
-		var novoAlbum = new album(nomeDoAlbum);
+		var novoAlbum = new Album(nomeDoAlbum, autor);
 		$scope.albuns.push(novoAlbum);
+		alert("Um novo album foi criado - TIRAR ALERT");
 		return novoAlbum;
 	}
 
 
 	//Adiciona uma musica a um album que está no array de albuns.
 	$scope.adicionaMusica = function(musica){
-		retornaAlbum(musica.album).pushMusic(angular.copy(musica));
+		album = retornaAlbum(musica.album, musica.autor);
+		album.pushMusic(angular.copy(musica));
 		delete musica;
 	}
+
+	//retorna um array de albuns de um determinado autor dado no parametro
+	$scope.retornaAlbuns = function(autor){
+		retorno = [];
+		for(i = 0; i < $scope.albuns.length; i++){
+			if($scope.albuns[i].autorDoAlbum == autor){
+				retorno.push($scope.albuns[i]);
+			}
+		}
+		return retorno;
+	}
+
+
 
 
 
